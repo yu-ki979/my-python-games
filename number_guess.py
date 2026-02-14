7745c7e12cf694954461fb22cc3898c1e26011da
your_name <ppine979@gmail.com>
2026-02-14 11:25:18 +0900

    数当て改良

--- number_guess.py
+++ number_guess.py
@@ -11,6 +11,9 @@ if "target" not in st.session_state:
     # 履歴を入れる「空のリスト」を作る
     st.session_state.history = []
 
+if "best_score" not in st.session_state:
+    st.session_state.best_score = float("inf") # 最初は無限大にしておく
+
 st.write("１から１０００の間で数字を当ててね！")
 
 # 入力ホームを作る
@@ -38,6 +41,12 @@ if st.button("予想する"):
         st.success(f"大正解！天才かよ！ 🎉 {st.session_state.count}回目で正解しました！")
         st.balloons()
 
+        # 今回のスコアがベストスコアより小さければ更新
+        if st.session_state.count < st.session_state.best_score:
+            st.session_state.best_score = st.session_state.count
+            st.write(f"🌟 自己ベスト更新！現在の記録：{st.session_state.best_score}回")
+        else:
+            st.write(f"現在の自己ベスト：{st.session_state.best_score}回")    
 # 履歴を画面に表示する
 if st.session_state.history:
     st.write("---")
